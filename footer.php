f36dd222e6c5d7032e9a3bda98d96ca2c6907dac
青山 <42113492+huhexian@users.noreply.github.com>
2023-08-06 22:30:46 +0800

    Update footer.php
    添加 instant.page 支持

--- footer.php
+++ footer.php
@@ -83,5 +83,6 @@
 		echo '</pre>-->';
 	}
 ?>
+<script src="//instant.page/5.2.0" type="module" integrity="sha384-jnZyxPjiipYXnSU0ygqeac2q7CVYMbh84q0uHVRRxEtvFPiQYbXWUorga2aqZJ0z"></script>
 </body>
 </html>
