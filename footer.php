cbe119fb003d40a38f346bb4e3b135927f8f6b0e
青山 <42113492+huhexian@users.noreply.github.com>
2023-12-02 10:01:14 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
 			<?php if ( ox_get_option( 'footerinfo_first' ) ) { echo ox_get_option( 'footerinfo_first' ); } ?>
-			<p>版权所有 &copy; (2018 - <?php echo date( "Y" ); ?>)  <a href="https://yinji.org">印记</a>│<a href="https://yinji.org/wp-sitemap.xml">网站地图</a>│服务器提供商: <a href="https://app.cloudcone.com/?ref=9587">CloudCone </a>.<br>程序：WordPress│主题：Twenty Twelve│本站禁止任何形式的文章转载<br><?php echo allwords(); ?>
+			<p>版权所有 &copy; (2018 - <?php echo date( "Y" ); ?>)  <a href="https://yinji.org">印记</a>│<a href="https://yinji.org/wp-sitemap.xml">网站地图</a>│服务器提供商: <a href="https://app.cloudcone.com/?ref=9587">CloudCone </a>.<br>程序：WordPress│主题：<a href="https://github.com/huhexian/2012-huhexian">Twenty Twelve</a>│本站禁止任何形式的文章转载<br><?php echo allwords(); ?>
 </p>
 		</div><!-- .site-info -->
 		<?php if ( ox_get_option( 'footerinfo' ) ) : ?>
